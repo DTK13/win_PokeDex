4faa786940ecece0277a63cd9348eef40d7db95d
Diyar Karim <diyarkarim@howardhanna.com>
2019-06-14 15:36:49 -0400

    Initial commit

---
+++ win_PokeDex/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace win_PokeDex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.txtNameSearch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPokeName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblPokeHeight = new System.Windows.Forms.Label();
+            this.lblPokeWeight = new System.Windows.Forms.Label();
+            this.imgPokemon = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbAbility = new System.Windows.Forms.ComboBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imgPokemon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.Font = new System.Drawing.Font("Britannic Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSearch.Location = new System.Drawing.Point(144, 425);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(167, 23);
+            this.lblSearch.TabIndex = 0;
+            this.lblSearch.Text = "Search Pokemon Name:";
+            this.lblSearch.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtNameSearch
+            // 
+            this.txtNameSearch.Location = new System.Drawing.Point(317, 428);
+            this.txtNameSearch.Name = "txtNameSearch";
+            this.txtNameSearch.Size = new System.Drawing.Size(497, 20);
+            this.txtNameSearch.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Britannic Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(144, 461);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Pokemon Name:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPokeName
+            // 
+            this.lblPokeName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblPokeName.Font = new System.Drawing.Font("Britannic Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPokeName.ForeColor = System.Drawing.Color.MediumBlue;
+            this.lblPokeName.Location = new System.Drawing.Point(317, 461);
+            this.lblPokeName.Name = "lblPokeName";
+            this.lblPokeName.Size = new System.Drawing.Size(497, 23);
+            this.lblPokeName.TabIndex = 3;
+            this.lblPokeName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Britannic Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(144, 496);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Pokemon Height (ft):";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Britannic Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(144, 533);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(167, 23);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Pokemon Weight (lb):\r\n";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPokeHeight
+            // 
+            this.lblPokeHeight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblPokeHeight.Font = new System.Drawing.Font("Britannic Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPokeHeight.ForeColor = System.Drawing.Color.MediumBlue;
+            this.lblPokeHeight.Location = new System.Drawing.Point(317, 496);
+            this.lblPokeHeight.Name = "lblPokeHeight";
+            this.lblPokeHeight.Size = new System.Drawing.Size(497, 23);
+            this.lblPokeHeight.TabIndex = 6;
+            this.lblPokeHeight.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPokeWeight
+            // 
+            this.lblPokeWeight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblPokeWeight.Font = new System.Drawing.Font("Britannic Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPokeWeight.ForeColor = System.Drawing.Color.MediumBlue;
+            this.lblPokeWeight.Location = new System.Drawing.Point(317, 533);
+            this.lblPokeWeight.Name = "lblPokeWeight";
+            this.lblPokeWeight.Size = new System.Drawing.Size(497, 23);
+            this.lblPokeWeight.TabIndex = 7;
+            this.lblPokeWeight.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // imgPokemon
+            // 
+            this.imgPokemon.BackColor = System.Drawing.Color.Silver;
+            this.imgPokemon.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.imgPokemon.Location = new System.Drawing.Point(12, 131);
+            this.imgPokemon.Name = "imgPokemon";
+            this.imgPokemon.Size = new System.Drawing.Size(999, 281);
+            this.imgPokemon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgPokemon.TabIndex = 8;
+            this.imgPokemon.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightGray;
+            this.button1.Font = new System.Drawing.Font("Britannic Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Location = new System.Drawing.Point(318, 618);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 48);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Britannic Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(144, 568);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(167, 23);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Abilities:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cbAbility
+            // 
+            this.cbAbility.FormattingEnabled = true;
+            this.cbAbility.Location = new System.Drawing.Point(318, 568);
+            this.cbAbility.Name = "cbAbility";
+            this.cbAbility.Size = new System.Drawing.Size(496, 21);
+            this.cbAbility.TabIndex = 12;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.LightGray;
+            this.btnClear.Font = new System.Drawing.Font("Britannic Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnClear.Location = new System.Drawing.Point(504, 618);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(164, 48);
+            this.btnClear.TabIndex = 13;
+            this.btnClear.Text = "Clear All";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(223, 183);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1024, 678);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.cbAbility);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imgPokemon);
+            this.Controls.Add(this.lblPokeWeight);
+            this.Controls.Add(this.lblPokeHeight);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblPokeName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNameSearch);
+            this.Controls.Add(this.lblSearch);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PokeDex";
+            ((System.ComponentModel.ISupportInitialize)(this.imgPokemon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.TextBox txtNameSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPokeName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblPokeHeight;
+        private System.Windows.Forms.Label lblPokeWeight;
+        private System.Windows.Forms.PictureBox imgPokemon;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbAbility;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ win_PokeDex/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace win_PokeDex
+{
+    public partial class Form1 : Form
+    {
+        //Set the Uri component in app settings by clicking on App.Config file
+        //Under configuration add the <appsettings> </appsettings> tags
+        //<add key="Url" value="https://pokeapi.co/api/v2/pokemon/"/> This will be the URL we make a request to get our data
+        //The Uri is then set to the Url in the appsettings
+        private string Uri = ConfigurationManager.AppSettings["Url"];
+
+        //This will be used to hold the user input in the textbox
+        private string pokemonName;
+
+        //This string will be used to hold our data response.
+        private string response = "";
+
+        private List<string> abilities = new List<string>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        
+        }
+
+        //Create a button, name it to whatever you want 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //We create an lblException label and set its visibilty to false so that it only appears in case of 
+            //an exception
+           
+
+            //The pokemon name has to be all lowercase so we set it by user .ToLower()
+            pokemonName = txtNameSearch.Text.ToLower();
+
+            //Put our web request in a try catch
+            try
+            {
+                //Creating our web request: A combination of our url plus the pokemon name
+                HttpWebRequest pokeRequest = (HttpWebRequest)WebRequest.Create(Uri + pokemonName);
+                //Specify the content we will get in response (JSON)
+                pokeRequest.ContentType = "application/json";
+                //Keep the request alive
+                pokeRequest.KeepAlive = true;
+                //Specify the method (GET)
+                pokeRequest.Method = "GET";
+
+                //Our response will be the request response
+                HttpWebResponse pokeResponse = (HttpWebResponse)pokeRequest.GetResponse();
+
+                //If our response returns an OK status code...Proceed. Else write the exception
+                if (pokeResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    //We use a streamreader to read our response to our empty string
+                    using (StreamReader sr = new StreamReader(pokeResponse.GetResponseStream()))
+                    {
+                        response = sr.ReadToEnd();
+                    }
+
+                    //We set the variable equal to deserialize the PokemonInfo. However we specify the RootObject subclass
+                    var pokemonContainer = JsonConvert.DeserializeObject<PokemonInfo.RootObject>(response);
+
+                    //Set the textboxes equal to the attribute of the class
+                    lblPokeName.Text = pokemonContainer.name.ToString();
+                    lblPokeHeight.Text = pokemonContainer.height.ToString();
+                    lblPokeWeight.Text = pokemonContainer.weight.ToString();
+
+                    imgPokemon.ImageLocation = pokemonContainer.sprites.front_default;
+                    //Using this webclient we download the client file (FOR FUN) then we set the image equal to the sprite image URL
+                 
+
+                    
+                    //We want to get all the moves the pokemon has/can learn into our ability combobox
+                    //So we iterate through each of the abilities using a foreach loop 
+                    //We go through and specify a variable to hold each object within the pokemoncontainer.moves
+                    foreach (var item in pokemonContainer.moves)
+                    {
+                        //To Add to a combobox we use the name then .Items then . then Add(item to add goes here)
+                        cbAbility.Items.Add(item.move.name.ToString());
+                        abilities.Add(item.move.name.ToString());
+                        
+                    }
+
+                    
+                    //Set the first selected item to the first item in the combobox!
+                    cbAbility.SelectedItem = cbAbility.Items[0];
+
+                    //We can call a function now to write everything to a Text file
+                    //Make sure to pass the information as parameters to our method
+                    WritePokemonToFile(pokemonContainer.name, pokemonContainer.height.ToString(), pokemonContainer.weight.ToString(),abilities,pokemonContainer.sprites.front_default);
+                }
+
+                //Else our exception message
+                else
+                {
+                    MessageBox.Show("Could not find the resource","Information", MessageBoxButtons.OKCancel,MessageBoxIcon.Error);
+                }
+
+
+
+
+            }
+
+            //else our other exception message
+            catch (Exception exception)
+            {
+
+                MessageBox.Show(exception.ToString(), "Error", MessageBoxButtons.OKCancel,MessageBoxIcon.Error);
+            }
+        }
+
+        //Our method to write pokemon info to text file  and download the image to a jpeg file
+        //Match the parameters in the places where you call this function
+        public void WritePokemonToFile(string pokemonName, string pokemonHeight, string pokemonWeight, List<string> abilities, string pokemonImage)
+        {
+            //Our file name for the text files will be right on our desktop
+            string filename = @"C:\Users\diyarkarim\desktop\" + pokemonName + ".txt";
+
+            //Using a streamwriter we can write all our information to our text file
+            //The true here means append any data, and false means delete and rewrite: Set to false
+            //We dont want a million of these...
+            using (StreamWriter sw = new StreamWriter(filename, false))
+            {
+                //Because we are using the Using statement no need to remember to close the writer or anything
+                sw.WriteLine("Pokemon Name: {0}", pokemonName);
+                //Lines are just for spacing
+                sw.WriteLine("===========================================");
+                sw.WriteLine("Pokemon Height (ft): {0} \r\n", pokemonHeight);
+                sw.WriteLine("Pokemon Weight (lb): {0} \r\n", pokemonWeight);
+                sw.WriteLine("===========================================");
+
+                //foreach each of our abilities write ability and the name of it
+                foreach (string item in abilities)
+                {
+                    sw.WriteLine("Ability: {0}", item);
+                }
+
+                sw.WriteLine("===========================================");
+
+
+            }
+
+            //Using our webclient we create an image file by using our image url then the path
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadFile(pokemonImage, @"C: \Users\diyarkarim\desktop\" + pokemonName + ".jpg");
+            }
+         
+
+            
+        }
+
+        //Clear Fields function
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtNameSearch.Text = string.Empty;
+            lblPokeHeight.Text = string.Empty;
+            lblPokeName.Text = string.Empty;
+            lblPokeWeight.Text = string.Empty;
+            imgPokemon.Image = null;
+            
+            for (int i = 0; i < cbAbility.Items.Count; i++)
+            {
+                cbAbility.Items[i] = string.Empty;
+                cbAbility.Items.Remove(i);
+            }
+
+            txtNameSearch.Focus();
+            
+        }
+    }
+}
